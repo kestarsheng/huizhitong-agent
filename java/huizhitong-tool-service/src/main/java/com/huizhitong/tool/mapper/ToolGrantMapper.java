3c6162e6ca481c223a5b16fa6efadd74db9253f4
kestarsheng <kktracy00230802@qq.com>
2026-08-21 10:06:34 +0800

    feat(java): 工具注册中心接入 MySQL 持久化

---
+++ java/huizhitong-tool-service/src/main/java/com/huizhitong/tool/mapper/ToolGrantMapper.java
@@ -0,0 +1,9 @@
+package com.huizhitong.tool.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.huizhitong.tool.domain.ToolGrant;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ToolGrantMapper extends BaseMapper<ToolGrant> {
+}
